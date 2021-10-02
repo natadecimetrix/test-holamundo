ad898e983254be43504d23fd365af89916a9a152
natadecimetrix <natalia.lenis@decimetrix.com>
2021-10-02 09:58:31 -0500

    First

---
+++ README.md
@@ -0,0 +1,3 @@
+# TRAINING
+
+## Nata demo
---
+++ train.py
@@ -0,0 +1,18 @@
+def calculate_distance (x1, y1, x2, y2):
+
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) 
+
+
+
+disntace1 = calculate_distance(1, 2, 3,4)
+
+print(disntace1)
+
+print(disntace1)
+
+
+print(disntace1)
+
+jls_extract_var = 123
+rgb=[255,jls_extract_var,1]
+rgb=[255,123,1]
